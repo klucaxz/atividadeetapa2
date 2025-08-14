c25fca5dac641b09868b996f25eb7264c0258f15
amostradinhoborabil <klucasvicente341@gmail.com>
2025-08-14 17:36:32 -0300

    1408

---
+++ atividadeetapa2/Assets/ControleDeAudio.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+using TMPro;
+public class ControleDeAudio : MonoBehaviour
+{
+    public int volume;
+    public int volumeSFX;
+    public bool musica;
+
+    public Slider volumeSlider;
+    public Slider volumeSFXSlider;
+    public Toggle toggleMusica;
+    public TMP_Text textoMusica; 
+
+    void Start()
+    {
+        musica = toggleMusica.isOn;
+        volume = (int)volumeSlider.value;
+        volumeSFX = (int)volumeSFXSlider.value;
+    }
+
+    void Update()
+    {
+        musica = toggleMusica.isOn;
+        volume = (int)volumeSlider.value;
+        volumeSFX = (int)volumeSFXSlider.value;
+
+        if (musica == true)
+        {
+            textoMusica.text = "Ligado";
+            textoMusica.color = Color.green;
+        }
+        else
+        {
+            textoMusica.text = "Desligado";
+            textoMusica.color = Color.red;
+        }
+    }
+}
